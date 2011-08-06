2c94bb7339af33a2512cf561e6c33f74eaad5967
Andre D <andre@paniccrew.de>
2011-08-06 11:00:58 +0200

    path

--- lib/rateable/helper.rb
+++ lib/rateable/helper.rb
@@ -2,11 +2,11 @@ module Rateable
   module Helper
     def rating_for(rateable, args = {}) 
       if @current_user and @current_user.ratings.where(:rateable => rateable).empty?
-        render :partial => "/rateable/rate", :locals => {:url => args[:url] ? args[:url] : url_for(rateable), :stars => args[:stars]}
+        render :partial => "rateable/rate", :locals => {:url => args[:url] ? args[:url] : url_for(rateable), :stars => args[:stars]}
       else
         stars_average = rateable.ratings.average("stars")
         ratings_count = rateable.ratings.count
-        render :partial => "/rateable/rating", :locals => {:stars => args[:stars], :rating => stars_average.to_i, :tooltip => "#{ratings_count} #{ratings_count == 1 ? "time" : "times"} rated, with an average value of #{stars_average}."}
+        render :partial => "rateable/rating", :locals => {:stars => args[:stars], :rating => stars_average.to_i, :tooltip => "#{ratings_count} #{ratings_count == 1 ? "time" : "times"} rated, with an average value of #{stars_average}."}
       end
     end
   end
