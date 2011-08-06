4fff08bc1238a684e26ea31b66d07902b92c14d9
Andre D <andre@paniccrew.de>
2011-08-06 09:27:26 +0200

    includes

--- lib/rateable.rb
+++ lib/rateable.rb
@@ -4,10 +4,13 @@ require "action_view"
 require 'rateable/is_rateable'
 require 'rateable/rater'
 require 'rateable/rate'
-ActiveRecord::Base.send :extend, Rateable::IsRateable
-ActiveRecord::Base.send :extend, Rateable::Rater
 
-if defined?(ActionView)
+if defined?(ActiveRecord::Base)
+  ActiveRecord::Base.send :include, Rateable::IsRateable
+  ActiveRecord::Base.send :include, Rateable::Rater
+end
+
+if defined?(ActionView::Base)
   require 'rateable/helper'
   ActionView::Base.send :include, Rateable::Helper
 end
