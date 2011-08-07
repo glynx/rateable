cdc931dba91648dcf92a8c6dbe13e8bc2618ceb1
Andre D <andre@paniccrew.de>
2011-08-07 15:54:48 +0200

    validations

--- lib/rateable/rate.rb
+++ lib/rateable/rate.rb
@@ -5,8 +5,9 @@ module Rateable
 
     validates_presence_of :rateable
     validates_presence_of :rater
+    validate :stars_must_be_in_range
 
-    def validate 
+    def stars_must_be_in_range 
       unless (1..rateable.max_stars).include? stars
         errors.add :stars, "must be within 1 to #{rateable.max_stars}"
       end
